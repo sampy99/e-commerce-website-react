76b0cf3249cbcc4cf4adec562982112edf43bb7b
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2023-12-17 23:48:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import {Navbar} from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>This is my Home</h1>
+      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/cart" />
+        </Routes>
+      </Router>
     </div>
   );
 }
