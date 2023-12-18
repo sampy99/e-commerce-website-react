5930986bd1141dc2558c566f26b134f4b99359a4
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2023-12-18 11:20:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-import {Navbar} from './Components/Navbar';
+import { Navbar } from './Components/Navbar';
+import { Shop } from './Pages/Shop/Shop';
+import { Cart } from './Pages/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is my Home</h1>
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" />
-          <Route path="/cart" />
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
     </div>
---
+++ src/Pages/Shop/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+  <div className='shop'>
+    <div className='shopTitle'>
+      <h1>Sampy Online Store</h1>
+    </div>
+    <div className='products'>
+
+    </div>
+  </div>
+  ) 
+}
+
+
