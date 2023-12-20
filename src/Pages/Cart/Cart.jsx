19d12354949013ed4623e822e9cc7824a64e122b
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2023-12-20 18:48:30 +0530

    commit

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -2,11 +2,11 @@ import React, { useContext } from 'react'
 import { PRODUCTS } from '../../Products'
 import { ShopContext } from '../../Context/ShopContext'
 import { CartItem } from './CartItem'
-
+import './Cart.css'
 
 export const Cart = () => {
 
-  const{addToCart,cartItems} = useContext(ShopContext)
+  const{cartItems} = useContext(ShopContext)
 
   return (
   <div className='cart'>
@@ -14,9 +14,9 @@ export const Cart = () => {
       <h1>Your Cart items</h1>
     </div>
     <div className='cartItems'>
-      {PRODUCTS.map((Product)=>{
-        if(cartItems[Product.id] !== 0){
-          return <CartItem data={Product} />
+      {PRODUCTS.map((product)=>{
+        if (cartItems[product.id] !== 0){
+          return <CartItem data={product} />
         }
       })}
     </div>
