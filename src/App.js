55184091e7b9e354da2b2bd3fb5cf6f816b1a86c
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2023-12-23 21:43:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import { Navbar } from './Components/Navbar';
+import { About } from './Components/About';
+import { Contact } from './Components/Contact';
 import { Shop } from './Pages/Shop/Shop';
 import { Cart } from './Pages/Cart/Cart';
 import { ShopContextProvider } from './Context/ShopContext';
@@ -14,6 +16,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
