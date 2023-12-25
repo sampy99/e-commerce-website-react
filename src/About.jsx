a26c0c71fcda27e60afee5ab6a6ad07ab6b47fa6
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2023-12-25 23:06:19 +0530

    commit

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import aboutimg from './Assets/A11.png';
+import './About.css'
 
 export const About = () => {
-  return <div>This is About</div>
-  
+  return(
+  <div className='about'>
+    <img src={ aboutimg } alt='about image' />
+    <h1>About Us</h1>
+  </div>
+  )  
 }
