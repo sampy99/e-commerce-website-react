8b1e8f7ee9bb9b2f4c858e451523f16272adb4c0
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2023-12-26 23:15:46 +0530

    commit

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import aboutimg from './Assets/A11.png';
 import steve from './Assets/A12.jpg'
+import steve1 from './Assets/A13.jpg'
+import steve2 from './Assets/A14.png'
 import './About.css'
 
 export const About = () => {
@@ -10,14 +12,38 @@ export const About = () => {
     <h1>About Us</h1>
 
     <div className='ceo'>
-    <div className='ceoimg'>
-     <h2>Apple Inc. / Founders</h2>
+      <h2>Apple Inc. / Founders</h2>
+      
+        <div className='ceo1'>
+    <div>
      <img src={steve} />
     </div>
-    <div className='ceodescription'>
+    <div className='ceo1description'>
      <h3>Steve Jobs</h3>
      <p>American Businessman</p>
     </div>
+        </div>
+
+       <div className='ceo2'>
+     <div>
+      <img src={steve1} />
+     </div>
+     <div className='ceo2description'>
+     <h3>Steve Wozniak</h3>
+     <p>Technology Entreprenur</p>
+     </div>
+        </div>
+
+        <div className='ceo3'>
+     <div>
+      <img src={steve2} />
+     </div>
+     <div className='ceo3description'>
+     <h3>Ronald Wayne</h3>
+     <p>Technology Entreprenur</p>
+     </div>
+        </div>
+
     </div>
   </div>
   )  
